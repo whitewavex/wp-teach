7accaf41d2a906818154677fe05bea235b2561ce
Alexandr <whitewavex@gmail.com>
2018-02-27 21:14:15 +0200

    Single Page without comments

--- single-.php
+++ single-.php
@@ -2,37 +2,31 @@
     <div class="publications">
         <div class="publications__container">
             <div class="container">
-               
-                <?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
-                
-                <?php 
-                    
-                    $categories = get_the_category();
-                    $previous_post = get_previous_post(true); 
-                    $next_post = get_next_post(true);
-                    $video = get_post_meta($post->ID, 'video', true);
-                    $presentation = get_post_meta($post->ID, 'show', true);
-                
-                ?>
-                
                 <div class="row justify-content-between publications__arrows arrows">
-                    <div class="col-xl-4 col-lg-5 col-md-6 col-sm-6 col-6 arrows__back">
-                        <?php
-                        
-                            if( $previous_post ) {
-                                echo '<a href="' . get_permalink( $previous_post ) . '" class="arrow"><i class="fa fa-arrow-left"></i><span class="arrows__text">попередній запис</span></a>';
-                            }
-                    
+                    <div class="col-xl-4 col-lg-5 col-md-7 col-sm-7 col-7 arrows__back">
+                        <?php $previous_post = get_previous_post();
+                            if( ! empty($previous_post) ){
+                        ?>
+                            <a href="<?php echo get_permalink( $previous_post->ID ); ?>" class="arrow">
+                            <i class="fa fa-arrow-left"></i>
+                            <span class="arrows__text">попередня публікація</span>
+                            </a>
+                            <?php
+                        }
                         ?>
                     </div>
-                    <div class="col-xl-4 col-lg-5 col-md-6 col-sm-6 col-6 arrows__next">
-                        <?php
-                        
-                            if( $next_post ) {
-                                echo '<a href="' . get_permalink( $next_post ) . '" class="arrow"><span class="arrows__text">наступний запис</span><i class="fa fa-arrow-right"></i></a>';
-                            }
-                    
+                    <div class="col-xl-4 col-lg-5 col-md-5 col-sm-5 col-5 arrows__next">
+                        <?php $next_post = get_next_post();
+                            if( ! empty($next_post) ){
                         ?>
+                                <a href="<?php echo get_permalink( $next_post->ID ); ?>" class="arrow">
+                                <span class="arrows__text">наступна публікація</span>
+                                <i class="fa fa-arrow-right"></i></a>
+                        <?php
+                            }
+                        ?>
+                        
+                        </a>
                     </div>
                 </div>
                 <div class="dots">
@@ -41,47 +35,27 @@
                     <div class="dot dots__item"></div>
                     <div class="dot dots__item"></div>
                 </div>
-                <main class="public">
-                    <h3 class="public__header public__header_rounded"><?php the_title(); ?></h3>
-                    <div class="public__content">
-                        <?php 
-                        
-                            if( $video ) {
-                                echo '<iframe class="public__video" src="' . $video . '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
-                            }
-                        
-                            if( $presentation ) {
-                                echo '<a target="_blank" href="' . $presentation . '">Перейти до перегляду презентації</a><hr>';
-                            }
-                        
-                        ?>
-                        <?php the_content(); ?>
-                    </div>
-                    <div class=" public__footer">
-                        <div class="date public__date">
-                            <i class="fa fa-calendar-check-o"></i>
-                            <span><?php the_date(); ?></span>
-                        </div>
-                        <p class="public__category">Категорія: 
-                        <?php 
-                            foreach( $categories as $category ){ 
-                                if( $category->category_description != '' ) {
-                                    continue;
-                                } 
-                                else {
-                                    $cat_name = $category->cat_name;
-                                    $cat_link = get_category_link( $category->cat_ID );
-                                    echo '<a class="public__link" href="' . $cat_link . '">' . $cat_name . '</a>'; 
-                                }
-                            }
-                        ?>
-                        </p>
-                    </div>
-                </main>
                 
+                <?php if (have_posts()) :  while (have_posts()) : the_post(); ?>
+                    <main class="public">
+                        <h3 class="public__header"><?php the_title(); ?></h3>
+                        <div class="public__content">
+                            <?php the_content(); ?>
+                        </div>
+                        <div class=" public__footer">
+                            <div class="date public__date">
+                                <i class="fa fa-calendar-check-o"></i>
+                                <span><?php the_date(); ?></span>
+                            </div>
+                            <div class="author public__author">
+                                <span>Автор: <?php the_author(); ?></span>
+                            </div>
+                        </div>
+                    </main>
                 <?php endwhile; ?>
                 <?php endif; ?>
-                
+
+<!--
                 <div class="comments main__comments">
                     <h4 class="comments__header">Оставить комментарий</h4>
                     <p class="comments__note">Ваш email не будет опубликован. Обязательные поля отмечены *</p>
@@ -112,7 +86,7 @@
                     <div class="comment">
                         <div class="row">
                             <div class="col-lg-1 col-sm-2">
-                                <img src="<?php bloginfo(template_url) ?>/images/comment_1.jpg" alt="">
+                                <img src="images/comment_1.jpg" alt="">
                             </div>
                             <div class="col-lg-11 col-sm-10">
                                 <h5 class="comment__header">Олександр</h5>
@@ -121,7 +95,7 @@
                                 <div class="comment">
                                     <div class="row">
                                         <div class="col-lg-1 col-sm-3">
-                                            <img src="<?php bloginfo(template_url) ?>/images/comment_2.jpg" alt="">
+                                            <img src="images/comment_2.jpg" alt="">
                                         </div>
                                         <div class="col-lg-11 col-sm-9">
                                             <h5 class="comment__header">Андрій</h5>
@@ -136,7 +110,7 @@
                     <div class="comment">
                         <div class="row">
                             <div class="col-lg-1 col-sm-2">
-                                <img src="<?php bloginfo(template_url) ?>/images/comment_2.jpg" alt="">
+                                <img src="images/comment_2.jpg" alt="">
                             </div>
                             <div class="col-lg-11 col-sm-10">
                                 <h5 class="comment__header">Андрій</h5>
@@ -146,6 +120,7 @@
                         </div>
                     </div>
                 </div>
+-->
             </div>
         </div>
     </div>
